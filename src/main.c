702ad7c385ae599e202df5d71adf0007d53ae112
clockworkcoding <maxm@clockworkcoding.com>
2013-05-30 22:33:55 -0700

    moved text layers

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 #define MY_UUID { 0x90, 0x68, 0xCE, 0xBD, 0x2E, 0x92, 0x4D, 0x2A, 0xAC, 0x83, 0xBE, 0xE1, 0x0A, 0xF0, 0xD9, 0x93 }
-PBL_APP_INFO(MY_UUID, "SOSMS", "Clockwork Coding", 0, 1, RESOURCE_ID_IMAGE_PLACEHOLDER_ICON,APP_INFO_STANDARD_APP);
+PBL_APP_INFO(MY_UUID, "SOSMS", "Clockwork Coding", 0, 2, RESOURCE_ID_IMAGE_PLACEHOLDER_ICON,APP_INFO_STANDARD_APP);
 
 #define MAX_MORSE_LENGTH 	6
 #define BUFFER_LENGTH 		50
@@ -26,7 +26,7 @@ char morseLetter[9]=""; //dots and dashes entered
 char letter[9]=""; // dots and dashes translated to 'i' and 't'
 
 char letterPreview[2]="";
-char msgLength[7]="   /160";
+char msgLength[7]="";
 
 char morse[][8] = {"i\0","ii\0","iii\0","iiii\0","iiiii\0","iiiit\0","iiit\0","iiitiit\0","iiitt\0","iit\0","iiti\0","iittii\0","iittit\0","iittt\0","it\0","iti\0","itii\0","itiii\0","itiiti\0","ititi\0","ititit\0","itt\0","itti\0","ittiti\0","ittt\0","itttt\0","itttti\0","t\0","ti\0","tiii\0","tii\0","tiiii\0","tiiiit\0","tiiit\0","tiit\0","tiiti\0","tit\0","titi\0","tititi\0","tititt\0","titt\0","titti\0","tittit\0","tt\0","tti\0","ttii\0","ttiii\0","ttiitt\0","ttit\0","ttt\0","tttii\0","tttiii\0","tttti\0","ttttt\0"};
 char characters[][2] = {"E\0","I\0","S\0","H\0","5\0","4\0","V\0","$\0","3\0","U\0","F\0","?\0","_\0","2\0","A\0","R\0","L\0","&\0","\"\0","+\0",".\0","W\0","P\0","@\0","J\0","1\0","'\0","T\0","N\0","B\0","D\0","6\0","-\0","=\0","X\0","/\0","K\0","C\0",";\0","!\0","Y\0","(\0",")\0","M\0","G\0","Z\0","7\0",",\0","Q\0","O\0","8\0",":\0","9\0","0\0"," \0"};
@@ -201,11 +201,11 @@ void click_config_provider(ClickConfig **config, Window *window) {
 void handle_init(AppContextRef ctx) {
   (void)ctx;
 
-  window_init(&window, "S.O.S.M.S.");
+  window_init(&window, "SOSMS");
   window_stack_push(&window, true /* Animated */);
 
   text_layer_init(&textLayer, GRect(2, 0, 140, 80));
-  text_layer_set_text(&textLayer, "...");
+  text_layer_set_text(&textLayer, "_");
   text_layer_set_background_color(&textLayer, GColorClear);
   text_layer_set_text_color(&textLayer, GColorBlack);
   text_layer_set_font(&textLayer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
@@ -219,7 +219,7 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_text_color(&morseCodeLayer, GColorBlack);
   layer_add_child(&window.layer, &morseCodeLayer.layer);
 
-  text_layer_init(&msgLengthLayer, GRect(0, 80, 55, 30));
+  text_layer_init(&msgLengthLayer, GRect(90, 80, 49, 30));
   text_layer_set_text(&msgLengthLayer, "0/160");
   text_layer_set_background_color(&msgLengthLayer, GColorClear);
   text_layer_set_text_color(&msgLengthLayer, GColorBlack);
@@ -227,7 +227,7 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_text_alignment(&msgLengthLayer, GTextAlignmentRight);
   layer_add_child(&window.layer, &msgLengthLayer.layer);
 	
-  text_layer_init(&letterPreviewLayer,GRect(120, 113, 144-40, 40));
+  text_layer_init(&letterPreviewLayer,GRect(120, 123, 144-40, 30));
   text_layer_set_text(&letterPreviewLayer, " ");
   text_layer_set_font(&letterPreviewLayer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_background_color(&letterPreviewLayer, GColorClear);
